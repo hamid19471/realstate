2404d58027ed2b7467449f5d6758c54b1aff6f80
Hamid Asadi <hamid.19471@gmail.com>
2023-09-24 11:18:56 +0330

    Create: responsive menu component and add to Header

---
+++ src/components/ResponsiveMenu/ResponsiveMenu.jsx
@@ -0,0 +1,37 @@
+import {
+  HomeIcon,
+  BuildingOfficeIcon,
+  EnvelopeIcon,
+  UsersIcon,
+} from "@heroicons/react/24/outline";
+import { NavLink } from "react-router-dom";
+
+const ResponsiveMenu = () => {
+  return (
+    <div className="sm:hidden fixed bottom-0 inset-0 flex items-end mb-4 justify-center mx-auto">
+      <ul className="menu gap-4 menu-horizontal bg-base-200 rounded-box">
+        <li>
+          <NavLink to="/" className="tooltip" data-tip="Home">
+            <HomeIcon className="h-5 w-5" />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" className="tooltip" data-tip="About">
+            <UsersIcon className="h-5 w-5" />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/hotels" className="tooltip" data-tip="Hotels">
+            <BuildingOfficeIcon className="h-5 w-5" />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact-us" className="tooltip" data-tip="Contact">
+            <EnvelopeIcon className="h-5 w-5" />
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default ResponsiveMenu;
