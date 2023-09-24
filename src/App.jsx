0cd245cb4f5e49bac08fedbd34c9653e7fc3bbc7
Hamid Asadi <hamid.19471@gmail.com>
2023-09-24 11:19:38 +0330

    Update: main routes of application

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Hotels from "./pages/Hotels/Hotels";
+import Contact from "./pages/Contact/Contact";
+
 const App = () => {
-  return <div>Home Page</div>;
+  return (
+    <div className="aj_container">
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/hotels" element={<Hotels />} />
+          <Route path="/contact-us" element={<Contact />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 };
 export default App;
