1172e80f2df9ad40d5e4a93d5c52175ab1ec34ff
Hamid Asadi <hamid.19471@gmail.com>
2023-09-24 20:00:25 +0330

    Update: Footer of main Layout

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,53 @@
 const Footer = () => {
-  return <div>this is Footer</div>;
+  return (
+    <div className="w-full py-20 md:pt-40 lg:px-32 overflow-hidden">
+      <div className=" flex flex-col md:flex-row items-start md:items-center justify-between p-2">
+        <div className="flex flex-col items-start w-[370px]">
+          <h1 className="font-bold text-3xl mb-3">
+            soge<span className="text-blue-700 font-bold text-3xl">King</span>
+          </h1>
+          <p>
+            SogeKing is the best place to buy and rent your dream home
+            throughout Indonesia
+          </p>
+        </div>
+        <div className="my-5 md:my-0">
+          <h3 className="font-bold text-xl mb-3">For Beginning</h3>
+          <ul className="flex flex-col gap-2 ">
+            <li className="footer_menu">About</li>
+            <li className="footer_menu">Career</li>
+            <li className="footer_menu ">Promotion</li>
+          </ul>
+        </div>
+        <div className="my-5 md:my-0">
+          <h3 className="font-bold text-xl mb-3">Overview</h3>
+          <ul className="flex flex-col gap-2 ">
+            <li className="footer_menu">Product</li>
+            <li className="footer_menu">Categories</li>
+            <li className="footer_menu ">Pricing</li>
+          </ul>
+        </div>
+        <div className="my-5 md:my-0">
+          <h3 className="font-bold text-xl mb-3">Explore Us</h3>
+          <ul className="flex flex-col gap-2 ">
+            <li className="footer_menu">Our Career</li>
+            <li className="footer_menu">Privacy</li>
+            <li className="footer_menu ">Cookie</li>
+          </ul>
+        </div>
+        <div className="my-5 md:my-0">
+          <h3 className="font-bold text-xl mb-3">Connect Us</h3>
+          <ul className="flex flex-col gap-2 ">
+            <li>support@sogeking.com</li>
+            <li>021 - 733 - 249</li>
+            <li>Serpong, South Tanggerang</li>
+          </ul>
+        </div>
+      </div>
+      <div className="text-center font-semibold py-6 mt-12">
+        <p>Copyright 2023 • All Rights Reserved SogeKing</p>
+      </div>
+    </div>
+  );
 };
 export default Footer;
