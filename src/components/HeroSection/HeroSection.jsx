9f2efcd6a9591e999ab98741d62739fc1c537f71
Hamid Asadi <hamid.19471@gmail.com>
2023-09-26 23:58:55 +0330

    Update: some styles issue for responsive

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -5,11 +5,11 @@ const HeroSection = () => {
         <h2 className="font-extrabold text-center lg:text-left text-5xl xl:text-7xl md:text-5xl lg:text-5xl w-full lg:w-[558px] xl:leading-[88px] mb-8">
           Let’s start the search for your dream home!
         </h2>
-        <p className="font-light md:text-justify w-full text-center lg:w-[443px]">
+        <p className="font-light lg:text-justify w-full text-center xl:w-[443px]">
           Your dream home awaits! This stunning property features all the
           amenities and comfort you desire.
         </p>
-        <div className="flex flex-col items-center justify-center lg:items-start gap-4 mt-20">
+        <div className="flex flex-col items-center justify-center lg:items-start gap-4 mt-8 xl:mt-20">
           <h3 className="font-bold text-2xl">Our Partner</h3>
           <div className="flex items-center justify-center space-x-4 lg:mt-7">
             <img src={"/public/images/Airbnb svg.png"} alt="" />
