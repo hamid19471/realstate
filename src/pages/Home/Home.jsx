7831f20fb51e28d7d9b8e0696db73c6f6c723f7c
Hamid Asadi <hamid.19471@gmail.com>
2023-10-02 00:56:42 +0330

    Update: import searchbox and testimonial component in home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import HeroSection from "../../components/HeroSection/HeroSection";
+import SearchBox from "../../components/SearchBox/SearchBox";
 import Subscription from "../../components/Subscription/Subscription";
+import Testimonials from "../../components/Testimonials/Testimonials";
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
+      <SearchBox />
+      <Testimonials />
       <Subscription />
     </div>
   );
