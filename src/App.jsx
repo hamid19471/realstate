f2f92648882d1f8deb243bf3c123f4d400504306
Hamid Asadi <hamid.19471@gmail.com>
2023-10-12 03:21:00 +0330

    Update: fix issue of useLocation, add other routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Hotels from "./pages/Hotels/Hotels";
 import Contact from "./pages/Contact/Contact";
+import SingleProperty from "./pages/SingleProperty/SingleProperty";
 import { useEffect } from "react";
+import SearchResult from "./pages/SearchResult/SearchResult";
 
 const App = () => {
-  //change page scroll to top on every page change
+  const location = useLocation();
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, []);
+  }, [location]);
   return (
     <div className="aj_container">
       <Routes>
@@ -18,6 +20,8 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/hotels" element={<Hotels />} />
+          <Route path="/search" element={<SearchResult />} />
+          <Route path="/:id" element={<SingleProperty />} />
           <Route path="/contact-us" element={<Contact />} />
         </Route>
       </Routes>
