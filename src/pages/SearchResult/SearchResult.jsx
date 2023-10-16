a9c264dd369e5931dba72c84016e776050922aa5
Hamid Asadi <hamid.19471@gmail.com>
2023-10-16 21:20:00 +0330

    Update: refactor component by context created

--- src/pages/SearchResult/SearchResult.jsx
+++ src/pages/SearchResult/SearchResult.jsx
@@ -1,19 +1,14 @@
-import { Link, useSearchParams } from "react-router-dom";
-import useFetch from "../../hooks/useFetch";
+import { Link } from "react-router-dom";
 import SearchCard from "../../components/SearchCard/SearchCard";
+import { useHotels } from "../../context/HotelsProvider";
+import Map from "../../components/Map/Map";
 const SearchResult = () => {
-  const [searchParams] = useSearchParams();
-  const location = searchParams.get("location");
-  const rooms = JSON.parse(searchParams.get("options"))?.rooms;
-  const { data, error, loading } = useFetch(
-    "http://localhost:3001/hotels",
-    `city_like=${location || ""}&accommodates_gte=${rooms || 1}`
-  );
+  const { data, error, loading } = useHotels();
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
   return (
     <div className="flex flex-col-reverse md:flex-row items-center justify-between my-12 gap-20 md:gap-16 lg:gap-12">
-      <div className="w-full">
+      <div className="w-full md:h-[650px] md:overflow-scroll">
         {data.map((item) => (
           <Link
             key={item.id}
@@ -23,7 +18,9 @@ const SearchResult = () => {
           </Link>
         ))}
       </div>
-      <div className="bg-slate-100 rounded-2xl p-12 w-full">Map</div>
+      <div className="bg-slate-100 rounded-2xl p-12 w-full ">
+        <Map />
+      </div>
     </div>
   );
 };
