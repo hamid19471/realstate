cac8ae50994ced32cebd6af5fd166bc983cfcc74
Hamid Asadi <hamid.19471@gmail.com>
2023-10-16 21:19:38 +0330

    Update: provide whole application by custom context

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import HotelsProvider from "./context/HotelsProvider.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <HotelsProvider>
+        <App />
+      </HotelsProvider>
     </React.StrictMode>
   </BrowserRouter>
 );
