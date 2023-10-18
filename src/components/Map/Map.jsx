06754df89b4d36381bca3943800bba62890dd73d
Hamid Asadi <hamid.19471@gmail.com>
2023-10-18 12:21:43 +0330

    Update: add sample and static map in component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,25 @@
-import { useHotels } from "../../context/HotelsProvider";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 const Map = () => {
-  const { data, error, loading } = useHotels();
-  return <div>this is Map</div>;
+  return (
+    <div className="h-screen">
+      <MapContainer
+        className="rounded-lg"
+        center={[51.505, -0.09]}
+        zoom={15}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 };
 export default Map;
