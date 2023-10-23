83c28f5d42d0fa1da20d9d952578ea4f72e1d343
Hamid Asadi <hamid.19471@gmail.com>
2023-10-23 23:52:49 +0330

    Update: fetch single hotels location on map, get permission for user location and set as default

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,28 +1,57 @@
-import { useState } from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useEffect, useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import { useHotels } from "../../context/HotelsProvider";
+import { useSearchParams } from "react-router-dom";
+import useYouserLocation from "../../hooks/useYouserLocation";
 
 const Map = () => {
   const { data, error, loading } = useHotels();
-  const [location, setLocation] = useState([51.505, -0.09]);
+  const [location, setLocation] = useState([20, 20]);
+  const [searchPraram, setSearchParams] = useSearchParams();
+  const lat = searchPraram.get("lat");
+  const lng = searchPraram.get("lng");
+  const {
+    loading: positionLoading,
+    userLocation,
+    getUserLocation,
+  } = useYouserLocation();
 
+  useEffect(() => {
+    if (lat && lng) setLocation([lat, lng]);
+  }, [lat, lng]);
+
+  useEffect(() => {
+    if (userLocation?.lat && userLocation?.lng) {
+      setLocation([userLocation?.lat, userLocation?.lng]);
+    }
+  }, [userLocation]);
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error</div>;
   return (
     <div className="h-screen">
       <MapContainer
         className="rounded-lg"
-        center={location}
-        zoom={15}
+        center={[lat || location[0], lng || location[1]]}
+        zoom={13}
         scrollWheelZoom={true}
       >
+        <button
+          onClick={getUserLocation}
+          className="bg-blue-700 p-1 px-3 shadow-lg shadow-blue-500 rounded-lg text-white absolute bottom-0 left-0 z-[999] m-2"
+        >
+          {positionLoading ? "Loading..." : "Your Location"}
+        </button>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={location}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {data.map((hotel) => {
+          return (
+            <Marker key={hotel.id} position={[hotel.latitude, hotel.longitude]}>
+              <Popup>{hotel.name}</Popup>
+            </Marker>
+          );
+        })}
       </MapContainer>
     </div>
   );
