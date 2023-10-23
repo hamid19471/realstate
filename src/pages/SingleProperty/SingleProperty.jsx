9145edebb1359a5b7ab22df9b0d365fd5405c200
Hamid Asadi <hamid.19471@gmail.com>
2023-10-23 23:55:15 +0330

    Update: add map component for single hotel component and set center map on it

--- src/pages/SingleProperty/SingleProperty.jsx
+++ src/pages/SingleProperty/SingleProperty.jsx
@@ -1,4 +1,16 @@
+import Map from "../../components/Map/Map";
+import { useHotels } from "../../context/HotelsProvider";
+
 const SingleProperty = () => {
-  return <div>this is SingleProperty</div>;
+  const { data, error, loading } = useHotels();
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error</div>;
+  return (
+    <div>
+      <div className="h-[600px] overflow-hidden">
+        <Map />
+      </div>
+    </div>
+  );
 };
 export default SingleProperty;
